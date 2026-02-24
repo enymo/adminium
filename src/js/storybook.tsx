bc54ba2491aa1bbe4b9556c491b98bfb82526f53
mimibun <166717259+mimibun@users.noreply.github.com>
2026-02-24 14:20:16 +0100

    add Filter, DotsDropdown and Breadcrumb components

--- src/js/storybook.tsx
+++ src/js/storybook.tsx
@@ -1,5 +1,9 @@
 import { configureIcons } from "@enymo/bcc";
+import i18next from "i18next";
+import { initReactI18next } from "react-i18next";
+import { MemoryRouter } from "react-router";
 import "../css/storybook.css";
+import de from "../lang/de.json";
 import CheckIcon from "../svg/check-solid-full.svg?react";
 import ChevronDown from "../svg/chevron-down-solid-full.svg?react";
 import CircleExclamationIcon from "../svg/circle-exclamation-solid-full.svg?react";
@@ -22,4 +26,29 @@ configureAdminIcons({
     filter: FilterIcon
 });
 
+i18next
+    .use(initReactI18next)
+    .init({
+        debug: true,
+        lng: "de",
+        fallbackLng: "de",
+        resources: {
+            de: {
+                translation: de
+            }
+        },
+        react: {
+            useSuspense: false
+        },
+        interpolation: {
+            escapeValue: false
+        }
+    });
+
 export const sleep = (duration: number) => new Promise<void>(resolve => setTimeout(resolve, duration));
+
+export const withMemoryRouter = (initialEntry?: string) => (Story: React.FC) => (
+    <MemoryRouter initialEntries={initialEntry ? [initialEntry] : undefined}>
+        <Story />
+    </MemoryRouter>
+)

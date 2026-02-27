42fe577a3a10ae0af7d95575b05abb8443841174
mimibun <166717259+mimibun@users.noreply.github.com>
2026-02-27 23:35:04 +0100

    feat: add exports to index.ts

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,5 +1,13 @@
+export { default as Badge } from "./components/Badge";
 export { default as Breadcrumbs } from "./components/Breadcrumbs";
 export { default as DotsDropdown } from "./components/DotsDropdown";
+export { default as EmailInput } from "./components/EmailInput";
+export { default as FileInput } from "./components/FileInput";
 export { default as Filter } from "./components/Filter";
+export { default as Frame } from "./components/Frame";
+export { default as Pagination } from "./components/Pagination";
+export { default as SelectMultiple } from "./components/SelectMultiple";
+export { default as Table } from "./components/Table";
+export { default as TableHeader } from "./components/TableHeader";
 export * from "./icons";
 

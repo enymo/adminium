c6fea257275cd891ea05f035b77d1088c716c624
mimibun <166717259+mimibun@users.noreply.github.com>
2026-03-10 12:18:40 +0100

    style: fix error to danger color

--- src/js/components/Badge.tsx
+++ src/js/components/Badge.tsx
@@ -11,7 +11,7 @@ export default function Badge({className, variant, children}: {
             "bg-primary-100 text-primary-700": variant === "primary",
             "bg-success-100 text-success-700": variant === "success",
             "bg-warning-100 text-warning-700": variant === "warning",
-            "bg-error-100 text-error-700": variant === "error"
+            "bg-danger-100 text-danger-700": variant === "error"
         }, className)}>{children}</div>
     )
 }

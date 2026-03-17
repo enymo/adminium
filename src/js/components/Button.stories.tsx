6dc9ae45bdce786b954fce4105ad4cd7bc985712
Gideon Ritter <gideon.ritter@enymo.com>
2026-03-17 13:36:40 +0100

    Minor fix

--- src/js/components/Button.stories.tsx
+++ src/js/components/Button.stories.tsx
@@ -1,6 +1,6 @@
 import type { Meta, StoryObj } from "@storybook/react-vite";
 import { fn } from "storybook/test";
-import { sleep } from "../storybook";
+import { sleep } from "../utilities";
 import Button from "./Button";
 
 const meta = {

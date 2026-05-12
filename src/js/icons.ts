23f3c57fc4ff6d02df757f406dd57930ac5cac9a
Gideon Ritter <gideon.ritter@enymo.com>
2026-05-12 14:35:01 +0200

    Fix icon adapter error message

--- src/js/icons.ts
+++ src/js/icons.ts
@@ -1,26 +1,29 @@
 export { Chevron as ChevronDown } from "@enymo/bcc";
 import type { Icon } from "@enymo/bcc";
 
+const missingIcons = () => {
+    throw new Error(`The icons for the adminium have not been configured! Please configure the icons by calling 'configureIcons' when booting your application`);
+}
 
-export let EllipsisVertical: Icon;
-export let FilterIcon: Icon;
-export let Gear: Icon;
-export let RightFromBracket: Icon;
-export let ChevronLeft: Icon;
-export let ChevronRight: Icon;
-export let ArrowUpRight: Icon;
-export let ArrowDownRight: Icon;
-export let ArrowUpLong: Icon;
-export let ArrowDownLong: Icon;
-export let XMark: Icon;
-export let MagnifyingGlass: Icon;
-export let ToastSuccess: Icon;
-export let ToastWarning: Icon;
-export let Paperclip: Icon;
-export let Download: Icon;
-export let Pdf: Icon;
-export let FaceSmile: Icon;
-export let PaperPlane: Icon;
+export let EllipsisVertical: Icon = missingIcons;
+export let FilterIcon: Icon = missingIcons;
+export let Gear: Icon = missingIcons;
+export let RightFromBracket: Icon = missingIcons;
+export let ChevronLeft: Icon = missingIcons;
+export let ChevronRight: Icon = missingIcons;
+export let ArrowUpRight: Icon = missingIcons;
+export let ArrowDownRight: Icon = missingIcons;
+export let ArrowUpLong: Icon = missingIcons;
+export let ArrowDownLong: Icon = missingIcons;
+export let XMark: Icon = missingIcons;
+export let MagnifyingGlass: Icon = missingIcons;
+export let ToastSuccess: Icon = missingIcons;
+export let ToastWarning: Icon = missingIcons;
+export let Paperclip: Icon = missingIcons;
+export let Download: Icon = missingIcons;
+export let Pdf: Icon = missingIcons;
+export let FaceSmile: Icon = missingIcons;
+export let PaperPlane: Icon = missingIcons;
 
 type IconName = "ellipsisVertical" | "filter" | "gear" | "rightFromBracket" | "chevronLeft" | "chevronRight" | "arrowUpLong" | "arrowDownLong" | "xMark" | "arrowUpRight" | "arrowDownRight" | "magnifyingGlass" | "toastSuccess" | "toastWarning" | "paperclip" | "download" | "pdf" | "facesmile" | "paperplane";
 export function configureAdminIcons({

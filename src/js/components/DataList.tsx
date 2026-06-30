4b589730db1d7d5ab1d5bc53196ea1f178fb13b4
Danae Ritter <danae.ritter@enymo.com>
2026-06-30 09:30:08 +0200

    Minor fix

--- src/js/components/DataList.tsx
+++ src/js/components/DataList.tsx
@@ -18,10 +18,10 @@ export function DataList({
                 {items.flatMap((items, i) => items.map(({key, label, value}, j) => (
                     <tr key={key}>
                         <td className={clsx("body-m text-text-800 pr-10", {
-                            [j > 0 ? "pt-2.5" : "pt-8"]: i > 0
+                            [j > 0 ? "pt-2.5" : "pt-8"]: i > 0 || j > 0
                         })}>{label}:</td>
                         <td className={clsx("body-m-md text-text-800", {
-                            [j > 0 ? "pt-2.5" : "pt-8"]: i > 0
+                            [j > 0 ? "pt-2.5" : "pt-8"]: i > 0 || j > 0
                         })}>{value}</td>
                     </tr>
                 )))}
